5a2b2c137e6d0126a4e87c3bbcd768e0be623dcf
altermulig <finn@altermulig.no>
2020-03-18 17:46:23 +0100

    getCurrentUser

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Individuals;
 use App\Entity\Users;
 use Symfony\Component\Sw;
@@ -12,7 +11,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Psr\Log\LoggerInterface;
 use \DateTime;
 use \App\Controller\SendEmailController;
-use \App\Controller\UtilController;
+
+header("Access-Control-Allow-Origin: *");
 
 class UserController extends AbstractController
 {
@@ -23,27 +23,13 @@ class UserController extends AbstractController
         $this->logger = $logger;
     }
 
-    /**
-     * @Route("/user", name="user")
-     */
-    public function index()
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
-    }
-
     public function registerUser(Request $request)
     {
         $bRegistreUser = false;
         $aReturn = array();
         /* STATUS Koder
          * 200 - OK
-         * 100 - Feil ved oppretting av bruker
-         * 101 - e-post finnes
-         * 102 - telefonnummer finnes
-         * 103 - Feil ved innlogging
+         * 400 - Generell feil
          */
         $this->logger->info($request);
 
@@ -71,7 +57,7 @@ class UserController extends AbstractController
         else { // bruker finnes
             //$this->logger->info("101");
             $bRegistreUser = false;
-            $aReturn[0] = 101;
+            $aReturn[0] = 400;
         }
 
         //Sjekke om telefon finnes fra før
@@ -92,7 +78,7 @@ class UserController extends AbstractController
         {
             //$this->logger->info("102");
             $bRegistreUser = false;
-            $aReturn[0] = 102;
+            $aReturn[0] = 400;
         }
 
         if ($bRegistreUser) {
@@ -160,8 +146,74 @@ class UserController extends AbstractController
         // sjekke passord.
         if ( ! password_verify($sPassword, $sHashPassword))
         {
-            $arrayCollection = array(103);
+            $arrayCollection = array(400);
             $this->logger->info('Feil ved innlogging');
+        }
+        else
+        {
+            // Generere en 20 char string som lagres på bruker. Brukes for å sjekke om det er riktig logginn ved hver api spørring.
+            $sAuthCode = UtilController::RandomString();
+            $arrayCollection[0]['authCode'] = $sAuthCode;
+
+            // lagre til databasen
+            /*$oUser2 = new Users();
+            $oUser2->setAuthCode($sAuthCode);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($oUser2);
+            $entityManager->flush();*/
+
+            $this->logger->info("AuthCode");
+            $this->logger->info($sAuthCode);
+        }
+
+        $this->logger->info(json_encode($arrayCollection));
+
+        return new JsonResponse($arrayCollection);
+    }
+
+    public function getCurrentUser(Request $request)
+    {
+        $bRegistreUser = false;
+        $aReturn = array();
+        /* STATUS Koder
+         * 200 - OK
+         * 400 - Generell feil
+         */
+        $this->logger->info($request);
+        $this->logger->info('getCurrentUser');
+
+        // Hente ut data fra overføring fra React
+        $content = json_decode($request->getContent());
+        $sUsername      = $content->email;
+
+        $oRepository = $this->getDoctrine()->getRepository(Users::class);
+
+        if(strpos($sUsername, "@") !== false) // logger inn med e-post
+            $oUser = $oRepository->findBy([ 'email' => $sUsername ]);
+
+        if ($oUser === null) {
+            $bRegistreUser = true;
+            $aReturn[0] = 400;
+
+        }
+        else
+        {
+            $bRegistreUser = false;
+            $aReturn[0] = 200;
+
+            $arrayCollection = array();
+
+            foreach ($oUser as $oU) {
+                $arrayCollection[] = array(
+                    'id' => $oU->getId(),
+                    'firstname' => $oU->getFirstname(),
+                    'middlename' => $oU->getMiddlename(),
+                    'lastname' => $oU->getLastname(),
+                    'email' => $oU->getEmail(),
+                    'mobile' => $oU->getPhone()
+                );
+            }
         }
 
         $this->logger->info(json_encode($arrayCollection));
