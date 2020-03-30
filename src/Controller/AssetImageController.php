01ef56e8849227c2de40fe76a9b9f76a5ea38ef7
jbg92 <jbg92@live.no>
2020-03-30 22:35:47 +0200

    la til billdeopplastning på asset og bildeurl til assetType

--- src/Controller/AssetImageController.php
+++ src/Controller/AssetImageController.php
@@ -32,13 +32,41 @@ class AssetImageController extends AbstractController{
     }
     public function addImage(Request $request, $userId, $assetId){
         $asset=$this->getDoctrine()->getRepository(Assets::class)->findOneBy(array('id'=>$assetId, 'users'=>$userId));
+        $imageAnt=$this->getDoctrine()->getRepository(AssetImages::class)->findBy(array('assets'=>$assetId))->count();
+        $imageAnt++;
+
+
         if(empty($asset)){
             return new JsonResponse($asset);
         }
 
         $content = json_decode($request->getContent());
-        $sImageUrl = $content->imageUrl;
+        $sImage = $content->image;
         $bMainImage = $content->mainImage;
+        $ImageOriginalName = $sImage->getClientOriginalName();
+
+        $aReturn['code'] = 400;
+        $aReturn['image'] = "";
+
+        // lage nutt bilde navn
+        $temp = explode(".", $ImageOriginalName);
+        $newfilename = $userId.'AssetImage.' .$imageAnt. end($temp);
+
+        $target_dir = "../../FrontEnd/public/AssetImages/";
+
+        $target_file = $target_dir . $newfilename;
+        $this->logger->info($target_file);
+
+        $check = getimagesize($sImage);
+        if($check !== false) {
+            //$this->logger->info("File is an image - " . $check["mime"] . ".");
+            $uploadOk = 1;
+        } else {
+            $this->logger->info("File is not an image.");
+            $uploadOk = 0;
+            // returnere 400 hvis det ikke er et bilde.
+            return new JsonResponse($aReturn);
+        }
 
         if($bMainImage){
             $assetImages=$this->getDoctrine()->getRepository(AssetImages::class)->findBy(array('assets'=>$assetId, 'mainImage'=>true));
@@ -51,25 +79,20 @@ class AssetImageController extends AbstractController{
             }
         }
 
-        $assetImage=new assetImages();
-        $assetImage->setAssets($asset);
-        $assetImage->setImageUrl($sImageUrl);
-        $assetImage->setMainImage($bMainImage);
+        if (move_uploaded_file($sImage, $target_file)) {
+            $aReturn['code'] = 200;
+            $aReturn['image'] = $newfilename;
+            $assetImage=new assetImages();
+            $assetImage->setAssets($asset);
+            $assetImage->setImageUrl($newfilename);
+            $assetImage->setMainImage($bMainImage);
 
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($assetImage);
-        $entityManager->flush();
-
-
-        return $this->json($assetImage, Response::HTTP_OK, [], [
-            ObjectNormalizer::SKIP_NULL_VALUES => true,
-            ObjectNormalizer::ATTRIBUTES => ['imageUrl'],
-            ObjectNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object) {
-                return $object->getId();
-            }
-        ]);
-
-
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($assetImage);
+            $entityManager->flush();
+        } else {
+            //$this->logger->info("Sorry, there was an error uploading your file.");
+        }
+        return new JsonResponse($aReturn);
     }
-
 }
