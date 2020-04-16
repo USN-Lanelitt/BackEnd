805c6f4fb2e27be736ccd0845dbc5700c73d73f9
jbg92 <jbg92@live.no>
2020-04-17 01:35:02 +0200

    ER SÅ LEI AV CORS

--- src/Controller/AssetController.php
+++ src/Controller/AssetController.php
@@ -16,14 +16,14 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Psr\Log\LoggerInterface;
 
 
-header("Access-Control-Allow-Origin: * ");
+/*header("Access-Control-Allow-Origin: * ");
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 $method = $_SERVER['REQUEST_METHOD'];
 if ($method == "OPTIONS") {
     die();
-}
+}*/
 
 
 class AssetController extends AbstractController{
--- src/Controller/RatingController.php
+++ src/Controller/RatingController.php
@@ -26,14 +26,14 @@ use Psr\Log\LoggerInterface;
 
 $request = Request::createFromGlobals();
 
-header("Access-Control-Allow-Origin: *");
+/*header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 $method = $_SERVER['REQUEST_METHOD'];
 if ($method == "OPTIONS") {
     die();
-}
+}*/
 
 class RatingController extends AbstractController{
 
